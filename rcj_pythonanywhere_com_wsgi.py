6b93039634d675ef2586a99958bf22a8039566fe
Manuel Bucher <dev@manuelbucher.com>
2019-12-01 18:50:38 +0000

    Changed to flask web app

--- rcj_pythonanywhere_com_wsgi.py
+++ rcj_pythonanywhere_com_wsgi.py
@@ -1,29 +1,16 @@
 # This file contains the WSGI configuration required to serve up your
-# web application at http://rcj.pythonanywhere.com/
+# web application at http://<your-username>.pythonanywhere.com/
 # It works by setting the variable 'application' to a WSGI handler of some
 # description.
 #
+# The below has been auto-generated for your Flask project
 
-# +++++++++++ GENERAL DEBUGGING TIPS +++++++++++
-# getting imports and sys.path right can be fiddly!
-# We've tried to collect some general tips here:
-# https://help.pythonanywhere.com/pages/DebuggingImportError
-
-# +++++++++++ VIRTUALENV +++++++++++
-# If you want to use a virtualenv, set its path on the web app setup tab.
-# Then come back here and import your application object as per the
-# instructions below
-
-
-# +++++++++++ CUSTOM WSGI +++++++++++
-# If you have a WSGI file that you want to serve using PythonAnywhere, perhaps
-# in your home directory under version control, then use something like this:
-#
 import sys
 
-path = '/home/rcj/rcj-server/src'
-if path not in sys.path:
-    sys.path.append(path)
+# add your project directory to the sys.path
+project_home = u'/home/rcj/rcj-server/src'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
 
-from wsgi import application  # noqa
-
+# import flask app but need to call it "application" for WSGI to work
+from flask_app import app as application  # noqa
